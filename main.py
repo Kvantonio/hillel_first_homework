500890d500d0a5f1d15e12a4f0018f57d786ac10
Kvantonio <kvantonio@i.ua>
2020-11-30 12:46:31 +0200

    Parse function

--- main.py
+++ main.py
@@ -1,5 +1,16 @@
-def parse(query: str) -> dict:
-    return {}
+def parse(url: str) -> dict:
+    d = {}
+    if len(url) == 0: return {}
+    if url.count('?') == 0: return {}
+    if url.split('?', 1)[1] == '': return {}
+    url = url.split('?')[1].split('&')
+    for i in range(len(url)):
+        if url[i] == '': continue
+        if url[i].count('=') == 0:
+            d[url[i].split('=', 1)[0]] = ""
+            continue
+        d[url[i].split('=', 1)[0]] = url[i].split('=', 1)[1]
+    return d
 
 
 if __name__ == '__main__':
@@ -9,13 +20,15 @@ if __name__ == '__main__':
     assert parse('http://example.com/?') == {}
     assert parse('http://example.com/?name=Dima') == {'name': 'Dima'}
 
+    assert parse('') == {}
+    assert parse('?') == {}
+    assert parse('&') == {}
+    assert parse('https://www.youtube.com/watch?v')
+    assert parse('https://www.youtube.com/watch?v=fgf&g&user=ghhfv') == {'v': 'fgf', 'g': '', 'user': 'ghhfv'}
+    assert parse('https://www.youtube.com/watch?v=1PHGmatiaME') == {'v': '1PHGmatiaME'}
+    assert parse('https://www.youtube.com/?v=1P&&user=ff') == {'v': '1P', 'user': 'ff'}
+    assert parse('https://www.youtube.com/watch?v=1PHGmatiaME=26s') == {'v': '1PHGmatiaME=26s'}
+    assert parse('https://www.youtube.com/watch?v=1PHGmatiaME=26s&n=h=j') == {'v': '1PHGmatiaME=26s', 'n':'h=j'}
+    assert parse('https://stepik.org/lesson/3380/step/2?unit=963') == {'unit': '963'}
+    assert parse('https://www.google.com/search?client=opera-gx&q=anaconda+import+requests&sourceid=opera') == {'client': 'opera-gx', 'q': 'anaconda+import+requests', 'sourceid': 'opera'}
 
-def parse_cookie(query: str) -> dict:
-    return {}
-
-
-if __name__ == '__main__':
-    assert parse_cookie('name=Dima;') == {'name': 'Dima'}
-    assert parse_cookie('') == {}
-    assert parse_cookie('name=Dima;age=28;') == {'name': 'Dima', 'age': '28'}
-    assert parse_cookie('name=Dima=User;age=28;') == {'name': 'Dima=User', 'age': '28'}
